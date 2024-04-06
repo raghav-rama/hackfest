54266a2459fa508c61753b2b71d0adf5c951d392
raghav-rama <hackerer528@gmail.com>
2024-04-06 17:59:42 +0530

    feat: add styling and minor fixes

--- members/static/js/metamask.js
+++ members/static/js/metamask.js
@@ -58,113 +58,121 @@ const abi = [
     type: "function",
   },
 ];
+if (typeof window.ethereum === "undefined") {
+  throw new Error("MetaMask is not installed or not properly configured");
+}
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(abi, contractAddress);
 (async () => {
   document
     .getElementById("connectButton")
     .addEventListener("click", async () => {
-      //check if Metamask is installed
       if (window.ethereum) {
         const web3 = new Web3(window.ethereum);
-        // Request the user to connect accounts (Metamask will prompt)
         await window.ethereum.request({ method: "eth_requestAccounts" });
-
-        // Get the connected accounts
         const accounts = await web3.eth.getAccounts();
-
-        // Display the connected account
-        document.getElementById("connectedAccount").innerText = accounts[0];
+        document.getElementById(
+          "connectButton"
+        ).innerText = `Connected: ${accounts[0].slice(
+          0,
+          6
+        )}...${accounts[0].slice(-4)}`;
+        document
+          .getElementById("contract-interaction")
+          .classList.remove("visually-hidden");
+        document
+          .getElementById("connect-to-metamask")
+          .classList.add("visually-hidden");
       } else {
-        // Alert the user to download Metamask
         alert("Please download Metamask");
       }
     });
-  const form = document.getElementById("addPackageForm");
-  console.log("form,", form);
-  form.addEventListener("submit", (e) => {
+  const addPackageForm = document.getElementById("addPackageForm");
+  addPackageForm.addEventListener("submit", async (e) => {
     try {
       e.preventDefault();
-      let web3;
-      if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
-      }
-      const contract = new web3.eth.Contract(abi, contractAddress);
-      const formData = new FormData(form);
+      const formData = new FormData(addPackageForm);
       const packageid = formData.get("packageid");
-      const initlocation = formData.get("initlocation");
+      const initloc = formData.get("initloc");
       const action = formData.get("action");
       const undertaken = formData.get("undertaken");
-      console.log(packageid, initlocation, action, undertaken);
-      if (typeof window.ethereum === "undefined") {
-        throw new Error("MetaMask is not installed or not properly configured");
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const contractFunction = {
+        inputs: [
+          {
+            internalType: "int256",
+            name: "packageid",
+            type: "int256",
+          },
+          {
+            internalType: "string",
+            name: "initlocation",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "action",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "undertaken",
+            type: "string",
+          },
+        ],
+        name: "addpackage",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      };
+      const functionArguments = [packageid, initloc, action, undertaken];
+      const encodedData = web3.eth.abi.encodeFunctionCall(
+        contractFunction,
+        functionArguments
+      );
+      const rawTransaction = {
+        from: accounts[0],
+        to: contractAddress,
+        value: 0,
+        maxFeePerGas: (await web3.eth.getBlock()).baseFeePerGas * 2n,
+        maxPriorityFeePerGas: 500,
+        gasLimit: 2000000,
+        nonce: await web3.eth.getTransactionCount(accounts[0]),
+        data: encodedData,
+      };
+      const tx = await web3.eth.sendTransaction(rawTransaction);
+      console.log(tx);
+      if (tx) {
+        const addPackageBtn = document.getElementById("addPackage");
+        const span = addPackageBtn.querySelector("span");
+        span.classList.add("visually-hidden");
       }
-      // Connect to the Ethereum network using MetaMask
-      (async () => {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        const web3 = new Web3(window.ethereum);
-        console.log("Get", await contract.methods.getpackage(2).call());
-        const contractFunction = {
-          inputs: [
-            {
-              internalType: "int256",
-              name: "packageid",
-              type: "int256",
-            },
-            {
-              internalType: "string",
-              name: "initlocation",
-              type: "string",
-            },
-            {
-              internalType: "string",
-              name: "action",
-              type: "string",
-            },
-            {
-              internalType: "string",
-              name: "undertaken",
-              type: "string",
-            },
-          ],
-          name: "addpackage",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        };
-        const functionArguments = [3, "Champa", "Delivered", "Yes"];
-        const encodedData = web3.eth.abi.encodeFunctionCall(
-          contractFunction,
-          functionArguments
-        );
-        const rawTransaction = {
-          from: accounts[0],
-          to: contractAddress,
-          value: 0,
-          maxFeePerGas: (await web3.eth.getBlock()).baseFeePerGas * 2n,
-          maxPriorityFeePerGas: 500,
-          gasLimit: 2000000,
-          nonce: await web3.eth.getTransactionCount(accounts[0]),
-          data: encodedData,
-        };
-        const tx = await web3.eth.sendTransaction(rawTransaction);
-        console.log(tx);
-        tx.on("sending", (sending) => {
-          console.log("Sending:", sending);
-        })
-          .on("sent", (sent) => {
-            console.log("Sent:", sent);
-          })
-          .on("transactionHash", (transactionHash) => {
-            console.log("Transaction Hash:", transactionHash);
-          })
-          .on("confirmation", (confirmation) => {
-            console.log("Confirmation:", confirmation);
-          })
-          .on("error", (error) => {
-            console.error("Error:", error);
-          });
-      })();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  const getPackageForm = document.getElementById("getPackageForm");
+  getPackageForm.addEventListener("submit", async (e) => {
+    try {
+      e.preventDefault();
+      const formData = new FormData(getPackageForm);
+      const packageid = formData.get("packageid");
+      const packageDetails = await contract.methods
+        .getpackage(packageid)
+        .call();
+      console.log(packageDetails);
+      if (packageDetails) {
+        const getPackageBtn = document.getElementById("getPackage");
+        const span = getPackageBtn.querySelector("span");
+        span.classList.add("visually-hidden");
+      }
+      const { 0: initloc, 1: action, 2: undertaken } = packageDetails;
+      document.getElementById("packageIdSpan").innerText = packageid;
+      document.getElementById("initLocSpan").innerText = initloc;
+      document.getElementById("actionSpan").innerText = action;
+      document.getElementById("undertakenSpan").innerText = undertaken;
     } catch (err) {
       console.log(err);
     }
